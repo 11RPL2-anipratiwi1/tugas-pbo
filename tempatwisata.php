ea4d92ced164521b47907c44161cc8d6e7c81f57
anipratiwi <anipratiwi0107@gmail.com>
2019-08-08 00:41:52 -0700

    Tugas script pantai dibuat Dwi

--- tempatwisata.php
+++ tempatwisata.php
@@ -49,3 +49,57 @@ $semeru->setNamagunung('semeru');
 $semeru->setKetinggian('3636');
 $semeru->setLetakgunung('jawatimur');
 $semeru->cetak();
+
+echo'<br>';
+
+
+class pantai{
+
+	var $namapantai;
+	var $kedalaman;
+	var $warnapasir;
+
+	public function setNamapantai($namapantai=''){
+		$this->namapantai = $namapantai;
+	}
+
+	public function setKedalaman($kedalaman=''){
+		$this->kedalaman = $kedalaman;
+	}
+
+	public function setwarnapasir($warnapasir=''){
+		$this->warnapasir = $warnapasir;
+	}
+
+	public function cetak(){
+		echo'namapantai:'.$this->namapantai;
+		echo'<br>';
+		echo'kedalaman:'.$this->kedalaman;
+		echo'<br>';
+		echo'warnapasir:'.$this->warnapasir;
+		echo'<br>';
+	}
+
+}
+
+$kartini = new pantai();
+$kartini->setNamapantai('kartini');
+$kartini->setKedalaman('1200');
+$kartini->setWarnapasir('putih');
+$kartini->cetak();
+
+echo'<br>';
+
+$telukawur = new pantai();
+$telukawur->setNamapantai('telukawur');
+$telukawur->setKedalaman('3500');
+$telukawur->setWarnapasir('hitam');
+$telukawur->cetak();
+
+echo'<br>';
+
+$bandengan = new pantai();
+$bandengan->setNamapantai('bandengan');
+$bandengan->setKedalaman('1500');
+$bandengan->setWarnapasir('coklat');
+$bandengan->cetak();
